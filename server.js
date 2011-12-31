e48136720f57f44507911cdd7c14631ea61e72e9
koma5 <marco@5th.ch>
2011-12-31 01:35:26 +0100

    added seccond server for broadcasting data to oher machines

--- server.js
+++ server.js
@@ -4,9 +4,22 @@ var carrier = require('carrier');
 var server = net.createServer(function(conn) {
   
   carrier.carry(conn, function(line) {
+    
+    connections.forEach(function(i) {
+        i.write(line);
+    })
     console.log(line);
   });
 
 });
 
 server.listen(6667);
+
+
+connections = [];
+
+broadcaster = net.createServer(function(conn) {
+  
+  connections.push(conn);  
+
+}).listen(6668);
